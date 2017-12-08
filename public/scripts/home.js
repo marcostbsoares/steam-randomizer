83f99ba61c751d1c5871a76e1581a950b8fdd43a
CptCoconut <markosthiago@gmail.com>
2017-12-08 20:55:28 -0200

    Added collapsible Achievement area to home page

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -3,3 +3,12 @@ $(".hint").popover();
 $(".hint").click(function(){
     $(this).popover();
 })
+
+$("#gauntlet_down_arrow").click(toggleAchievDiv);
+$("#gauntlet_div").hide(1);
+
+function toggleAchievDiv(){
+    $("#gauntlet_div").slideToggle(500, function(obj){
+        $("#gauntlet_down_arrow").toggleClass("flip_vertical")
+    });
+}
