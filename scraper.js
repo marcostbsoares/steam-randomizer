24f9329c1c67216fe16d5ea7b157b7a77b0aa27c
CptCoconut <markosthiago@gmail.com>
2018-01-20 02:45:26 -0200

    Add config json for shared data access

--- scraper.js
+++ scraper.js
@@ -9,11 +9,11 @@ var express = require("express")
 // INITIALIZE SETTINGS AND VARIABLES
 ///////////////////////
 
-var apiKey = "A7D0B730D9B110FE11D87D6BD2975589";
-
+var config = JSON.parse(fs.readFileSync("config.json"));
+var apiKey = config.apiKey;
 
 //Connect to DataBase
-mongoose.connect("mongodb://localhost/steam_rand");
+mongoose.connect(config.dbConnection);
 
 
 //Create Schemas and Models
@@ -25,18 +25,23 @@ var GameSchema = new mongoose.Schema({
 
 var Games = mongoose.model("Game", GameSchema);
 
-var GameSchemaSchema = new mongoose.Schema({
+var GameDataSchema = new mongoose.Schema({
     gameName: String,
     gameVersion: Number,
     app_id: Number,
-    availableGameStats: 
-     {
-        achievements: [ { name: String, defaultvalue: Number, description: String, hidden: Number, icon: String, icongray: String}]
-     },
+    achievements: [ { name: String, defaultvalue: Number, description: String, hidden: Number, icon: String, icongray: String}],
+    hasAchievements: Boolean,
 });
 
+var GameData = mongoose.model("GameSchema", GameDataSchema);
 
-//Retrieve API data and add new entries to DB
+var NonGameAppSchema = new mongoose.Schema({
+    app_id: Number,
+});
+
+var NonGameApp = mongoose.model("NonGameApp", NonGameAppSchema);
+
+//Lists all games and apps in steam and add new entries to DB
 function RetrieveApiGameData()
 {
     request("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=" + apiKey + "&format=json", function(err, response)
@@ -51,6 +56,7 @@ function RetrieveApiGameData()
                 {
                     if(err || res == null)
                     {
+                        //Create new entry if none was found matching the requested app id
                         var newObj = { name: app.name, app_id: app.appid, achievs_parsed: false };
                         Games.create(newObj, function(err, resp)
                         {
@@ -66,9 +72,76 @@ function RetrieveApiGameData()
     })
 }
 
-function ParseSchemas(){
-    
+async function ParseSchemas(){
+    //Get list of all games from DB
+    Games.find(async function(err, res)
+    {
+        if(!err && res !== null)
+        {
+            //For each game in Games collection
+            for(let i = 0; i < res.length; i+= 10)
+            {
+                //TODO: Optimize for parallel requests
+                let promises = [];
+                for(let n = 0; n < 10; n++)
+                {
+                    promises[n] = GetGameData(res[i + n]);
+                }
+
+                for (let n = 0; n < 10; n++) {
+                    await promises[n];
+                }
+            }
+        }
+    });
 }
 
-RetrieveApiGameData();
+async function GetGameData(model)
+{
+    //Check if GameDataSchema exists on database for that app id
+    let dbResponse = null;
+
+    //Search in non-game app list
+    await NonGameApp.findOne({ "app_id": model.app_id }, function (err, res) {
+        dbResponse = res;
+        return;
+    });
+
+    //Search in game list if none were found
+    if(dbResponse == null) await GameData.findOne({ "app_id": model.app_id }, function (err, res) {
+        dbResponse = res;
+        return;
+    });
+
+    if(dbResponse == null) await request("http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=" + apiKey + "&appid=" + model.app_id, function (err, response) {
+        if (!err && response != null) {
+            try
+            {
+                let resp = JSON.parse(response.body).game;
+                if (Object.keys(resp).length == 0)
+                {
+                    //console.log("Empty Object");
+                    NonGameApp.create({ app_id : model.app_id});
+                }
+                else
+                {
+                    let newGameData = { gameName: resp.gameName, gameVersion: resp.gameVersion, app_id : model.app_id };
+                    if (resp.availableGameStats != null && resp.availableGameStats.achievements != null) 
+                    {
+                        newGameData.achievements = resp.availableGameStats.achievements;
+                        newGameData.hasAchievements = true;
+                    }
+                    else { newGameData.hasAchievements = false; }
+                    GameData.create(newGameData);
+                    console.log("New GameData entry created with id " + newGameData.app_id);
+                }
+            }
+            catch(exception){
+                //console.log("Error parsing game with id " + model.app_id);
+            }
+        }
+    }).catch(function(err) { console.log(err)});
+}
+
+//RetrieveApiGameData();
 ParseSchemas();
