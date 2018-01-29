751084bc14307370a54d03726b28b0aaf8942b9e
CptCoconut <markosthiago@gmail.com>
2018-01-28 22:09:37 -0200

    Added Achievement Data retrieval to scraper utility

--- scraper.js
+++ scraper.js
@@ -20,7 +20,7 @@ mongoose.connect(config.dbConnection);
 var GameSchema = new mongoose.Schema({
     name: String,
     app_id: Number,
-    achievs_parsed : Boolean,
+    achievs_parsed: Boolean,
 });
 
 var Games = mongoose.model("Game", GameSchema);
@@ -29,7 +29,7 @@ var GameDataSchema = new mongoose.Schema({
     gameName: String,
     gameVersion: Number,
     app_id: Number,
-    achievements: [ { name: String, defaultvalue: Number, description: String, hidden: Number, icon: String, icongray: String}],
+    achievements: [{ name: String, defaultvalue: Number, description: String, hidden: Number, icon: String, icongray: String, achievementDifficulty: String, percentCompleted: Number }],
     hasAchievements: Boolean,
 });
 
@@ -42,25 +42,18 @@ var NonGameAppSchema = new mongoose.Schema({
 var NonGameApp = mongoose.model("NonGameApp", NonGameAppSchema);
 
 //Lists all games and apps in steam and add new entries to DB
-function RetrieveApiGameData()
-{
-    request("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=" + apiKey + "&format=json", function(err, response)
-    {
-        if(!err)
-        {
+function retrieveApiGameData() {
+    request("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=" + apiKey + "&format=json", function (err, response) {
+        if (!err) {
             var resp = JSON.parse(response.body);
-            resp.applist.apps.forEach(app => 
-            {
+            resp.applist.apps.forEach(app => {
                 //Check if entry exists in database
-                Games.findOne({"app_id" : app.appid}, function(err, res)
-                {
-                    if(err || res == null)
-                    {
+                Games.findOne({ "app_id": app.appid }, function (err, res) {
+                    if (err || res == null) {
                         //Create new entry if none was found matching the requested app id
                         var newObj = { name: app.name, app_id: app.appid, achievs_parsed: false };
-                        Games.create(newObj, function(err, resp)
-                        {
-                            if(!err)
+                        Games.create(newObj, function (err, resp) {
+                            if (!err)
                                 console.log("Object Created!" + resp);
                             else
                                 console.log(err);
@@ -72,22 +65,19 @@ function RetrieveApiGameData()
     })
 }
 
-async function ParseSchemas(){
+async function parseSchemas() {
     //Get list of all games from DB
-    Games.find(async function(err, res)
-    {
-        if(!err && res !== null)
-        {
+    Games.find(async function (err, res) {
+        if (!err && res !== null) {
             //For each game in Games collection
-            for(let i = 0; i < res.length; i+= 10)
-            {
-                //TODO: Optimize for parallel requests
+            for (let i = res.length; i >= 0; i -= 10) {
                 let promises = [];
-                for(let n = 0; n < 10; n++)
-                {
-                    promises[n] = GetGameData(res[i + n]);
+                for (let n = 0; n < 10; n++) {
+                    //Start batch of simultaneous requests
+                    promises[n] = getGameData(res[i + n]);
                 }
 
+                //Await for batch to finish before starting a new one
                 for (let n = 0; n < 10; n++) {
                     await promises[n];
                 }
@@ -96,8 +86,7 @@ async function ParseSchemas(){
     });
 }
 
-async function GetGameData(model)
-{
+async function getGameData(model) {
     //Check if GameDataSchema exists on database for that app id
     let dbResponse = null;
 
@@ -108,26 +97,25 @@ async function GetGameData(model)
     });
 
     //Search in game list if none were found
-    if(dbResponse == null) await GameData.findOne({ "app_id": model.app_id }, function (err, res) {
+    if (dbResponse == null) await GameData.findOne({ "app_id": model.app_id }, function (err, res) {
         dbResponse = res;
         return;
     });
 
-    if(dbResponse == null) await request("http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=" + apiKey + "&appid=" + model.app_id, function (err, response) {
+    //Request game schema information from Steam API
+    if (dbResponse == null) await request("http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=" + apiKey + "&appid=" + model.app_id, function (err, response) {
         if (!err && response != null) {
-            try
-            {
+            try {
                 let resp = JSON.parse(response.body).game;
-                if (Object.keys(resp).length == 0)
-                {
-                    //console.log("Empty Object");
-                    NonGameApp.create({ app_id : model.app_id});
+                //If object does not contain game data, store in NonGameApp DB
+                if (Object.keys(resp).length == 0) {
+                    NonGameApp.create({ app_id: model.app_id });
                 }
-                else
-                {
-                    let newGameData = { gameName: resp.gameName, gameVersion: resp.gameVersion, app_id : model.app_id };
-                    if (resp.availableGameStats != null && resp.availableGameStats.achievements != null) 
-                    {
+                else {
+                    //Create new GameData DB Entry for a new game
+
+                    let newGameData = { gameName: resp.gameName, gameVersion: resp.gameVersion, app_id: model.app_id };
+                    if (resp.availableGameStats != null && resp.availableGameStats.achievements != null) {
                         newGameData.achievements = resp.availableGameStats.achievements;
                         newGameData.hasAchievements = true;
                     }
@@ -136,12 +124,86 @@ async function GetGameData(model)
                     console.log("New GameData entry created with id " + newGameData.app_id);
                 }
             }
-            catch(exception){
-                //console.log("Error parsing game with id " + model.app_id);
+            catch (exception) {
+
             }
         }
-    }).catch(function(err) { console.log(err)});
+    }).catch(function (err) { console.log(err) });
+}
+
+//Obtain Achievement percentages from API and update existing achievements to reflect that
+async function retrieveAchievementPercentages() {
+
+    let cursor = GameData.find({ "hasAchievements": true }).cursor();
+    let finished = false;
+
+    while(!finished)
+    {
+        var doc = null;
+        await cursor.next(function(err, obj)
+        {
+            if(!err)
+            {
+                doc = obj;
+                if(obj == null)
+                {
+                    finished = true;
+                }
+            }
+        });
+
+        if(finished)
+            break;
+
+        let apiResult = null;
+        await request("http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=" + doc.app_id, function (error, response) {
+            if (!error)
+                apiResult = JSON.parse(response.body);
+            else
+                console.log(error);
+        }).catch(function(error)
+        { 
+            console.log("Error parsing APP with ID " + doc.app_id + " / " + doc.gameName);
+            console.log(error); 
+        });
+
+        if(apiResult == null || apiResult.achievementpercentages == undefined)
+            continue;
+
+        let newAchievArray = [];
+        
+        doc.achievements.forEach(achiev => {
+            let resultAchiev = apiResult.achievementpercentages.achievements.find( el => {return el.name == achiev.name});
+            if(resultAchiev == undefined)
+                return;
+
+            let percent = resultAchiev.percent.toFixed(2);
+            let difficulty = (percent <= 5) ? "EXPERT" : (percent <= 15) ? "HARD" : (percent <= 30) ? "NORMAL" : "EASY";
+            let updatedAchievData = 
+            {
+                name: achiev.name, 
+                defaultvalue: achiev.defaultvalue, 
+                description: achiev.description, 
+                hidden: achiev.hidden, 
+                icon: achiev.icon, 
+                icongray: achiev.icongray, 
+                achievementDifficulty: difficulty,
+                percentCompleted: percent,
+
+            };
+            newAchievArray.push(updatedAchievData);
+        });
+
+        GameData.findByIdAndUpdate(doc._id, { achievements: newAchievArray}, function(err, res){
+            if(doc != null)
+                console.log("Game achievs parsed for " + doc.gameName);
+            else
+                console.log("WHAT THE FUCK");
+        });
+    };
+    console.log("FINISHED");
 }
 
 //RetrieveApiGameData();
-ParseSchemas();
+//ParseSchemas();
+retrieveAchievementPercentages();
